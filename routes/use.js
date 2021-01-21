118d56399381bf849065d91b5ce16e3d8c86348a
din730199 <46752162+din730199@users.noreply.github.com>
2021-01-21 20:36:29 +0700

    Update use.js

--- routes/use.js
+++ routes/use.js
@@ -27,4 +27,37 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    // Find user by id
+    let user = await User.findById(req.params.id);
+    // Delete image from cloudinary
+    await cloudinary.uploader.destroy(user.cloudinary_id);
+    // Delete user from db
+    await user.remove();
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.put("/:id", upload.single("image"), async (req, res) => {
+  try {
+    let user = await User.findById(req.params.id);
+    // Delete image from cloudinary
+    await cloudinary.uploader.destroy(user.cloudinary_id);
+    // Upload image to cloudinary
+    const result = await cloudinary.uploader.upload(req.file.path);
+    const data = {
+      name: req.body.name || user.name,
+      avatar: result.secure_url || user.avatar,
+      cloudinary_id: result.public_id || user.cloudinary_id,
+    };
+    user = await User.findByIdAndUpdate(req.params.id, data, { new: true });
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = router;
